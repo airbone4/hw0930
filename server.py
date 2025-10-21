6db76e11aa51e3b19b5074627ce784b27d1f4864
airways1 <airways1@users.noreply.github.com>
2025-10-21 06:39:05 +0000

    server ok

--- server.py
+++ server.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    # 由 static 資料夾提供 Iindex.html
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
